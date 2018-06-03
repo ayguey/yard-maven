c82577c8d9280e3eed7d1379096ed381c2655db0
muripo <yappamuripo@gmail.com>
2018-06-03 11:22:46 -0700

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+   $(".navText").hover(function() {
+        $(this).addClass("navHover");
+    }, function() {
+        $(this).removeClass("navHover"); 
+    }); 
+});
